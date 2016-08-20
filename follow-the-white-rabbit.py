60a06057d36b0fdeac9382a15b568f233010bdb2
pnbt <chaslot.guillaume@gmail.com>
2016-08-20 18:25:45 +0200

    Add description

--- follow-the-white-rabbit.py
+++ follow-the-white-rabbit.py
@@ -1,5 +1,15 @@
 __author__ = 'Guillaume Chaslot'
 
+"""
+    This scripts starts from a search query on youtube and:
+        1) gets the N first search results
+        2) follows the first M recommendations
+        3) repeats step (2) P times
+        4) stores the results in a json file
+"""
+
+
+
 import urllib2
 import re
 import json
